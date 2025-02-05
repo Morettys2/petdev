ae0b29db40326e09468d5c9975da8d019f639c93
Joao Pedro Moretti <m0rettyy20@gmail.com>
2025-02-05 13:22:57 -0300

    Começo do projeto

---
+++ src/app/_components/hero.tsx
@@ -0,0 +1,37 @@
+import { WhatsappLogo } from '@phosphor-icons/react/dist/ssr'
+
+export function Hero() {
+    return (
+        <section className="bg-[#e84c3d] text-white relative overflow-hidden">
+            <div className='countainer mx-auto '> 
+                <article className='grid grid-cols-1 lg:grid-cols-2 gap-8 relative  '>
+                    <div className='space-y-6 '>
+                        <h1 className="text-3xl font-bold md:text-4xl lg:text-5xl leading-10">
+                            Seu pet merece cuidado, carinho e atenção especial.
+                        </h1>
+                        <p className="lg:text-lg">
+                            Oferecemos os melhores serviços para garantir o bem-estar e a felicidade do seu amigo de quatro patas.
+                        </p>
+                        <div className="flex">
+                            <a
+                                href="#"
+                                className="bg-green-500 px-5 py-2 rounded-md font-semibold flex items-center justify-center gap-2"
+                            >
+                                <WhatsappLogo className='w-5 h-5' />
+                                Contato via WhatsApp
+                            </a>
+                        </div>
+                        <div className="mt-8">
+                            <p className="text-sm mb-4">
+                                <b className="bg-black text-white px-2 py-1 rounded-md">5%</b> de desconto na primeira compra.
+                            </p>
+                        </div>
+                    </div>
+                    <div>
+                        <h1>Foto</h1>
+                    </div>
+                </article>
+            </div>
+        </section>
+    )
+}
