b14e362c1fe4308f5cd3cb4b4f7888959fa62c91
Joao Pedro Moretti <m0rettyy20@gmail.com>
2025-02-06 16:28:40 -0300

    Finalizado o services

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import { About } from "./_components/about";
 import { Hero } from "./_components/hero";
+import { Services } from "./_components/services";
+
 
 
 export default function Home() {
   return (
     <main>
-      <Hero/>
-      <About/>
+      <Hero />
+      <About />
+      <Services />
     </main>
   );
 } 
